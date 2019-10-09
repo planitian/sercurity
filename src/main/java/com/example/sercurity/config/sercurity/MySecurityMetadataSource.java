2665f7ba75cd814f6f391451107e03cac4f87e7c
plani <596971449@qq.com>
2019-10-09 09:42:04 +0800

    SecurityMetadataSource 方法 添加描述

--- src/main/java/com/example/sercurity/config/sercurity/MySecurityMetadataSource.java
+++ src/main/java/com/example/sercurity/config/sercurity/MySecurityMetadataSource.java
@@ -20,7 +20,15 @@ public class MySecurityMetadataSource implements FilterInvocationSecurityMetadat
      * 这一部分 我没有写 具体的代码，因为 至少公司项目层面没用到，这里 我只说一下用法
      * 最好参照 默认实现类的代码  很棒
      *
-     * @param object 就是controller 层的类及其方法，和 静态资源等
+     * @param object FilterSecurityInterceptor 时候  object 为FilterInvocation
+     *
+     *               MethodSecurityInterceptor   时候  为  MethodInvocation，
+     *               AbstractMethodSecurityMetadataSource为所有注解的父类，例如  @Secured({"saas_org_basic_joining"})等
+     *               会先调用 AbstractMethodSecurityMetadataSource 的getAttributes(Object object)方法 ，通过下面的代码
+     *               再调用 具体注解类的 重载方法
+     *               Collection<ConfigAttribute> attrs = getAttributes(mi.getMethod(), targetClass);
+     *
+     *
      * @return
      * @throws IllegalArgumentException
      */
@@ -36,6 +44,7 @@ public class MySecurityMetadataSource implements FilterInvocationSecurityMetadat
                 return entry.getValue();
             }
         }*/
+
         //这里 我随便 返回一两个
         Set<ConfigAttribute> allAttributes = new HashSet<>();
         allAttributes.add(new ConfigAttribute() {
